a97f7dbbe21d1630f265f8fbf2f25420a2b61e22
Ceedz1 <84215937+Ceedz1@users.noreply.github.com>
2022-11-12 21:37:40 +0800

    update3

--- Abash/Atbash.c
+++ Abash/Atbash.c
@@ -3,45 +3,55 @@
 #include <string.h>
 #include <conio.h>
 
-//-------------------------------------------------------------
+//Encrypt Function-------------------------------------------------------------
 
 char* funcEncpt(char plaintext[]){
   int i, a;
   char ch;
 
+  //dynamically allocating "encrypted"
   char* encrypted = malloc(sizeof(encrypted)*sizeof(char*));
+
+  //convert to ascii value
   for(i = 0; i < strlen(plaintext); i++){
     a=plaintext[i];
     a=91-(a-64);
     ch=a;
     encrypted[i] = ch;
   }
-
+  
+  //return decrypted value
   return encrypted;
 }
 
-//-------------------------------------------------------------
+//Decrypt Function-------------------------------------------------------------
 
 char* funcDecpt(char encpt[]){
   int i, a;
   char ch;
 
+  //dynamically allocating "decrypted"
   char* decrypted = malloc(sizeof(decrypted)*sizeof(char*));
+
+  //convert to ascii value
   for(i = 0; i < strlen(encpt); i++){
     a=encpt[i];
     a=91-(a-64);
     ch=a;
     decrypted[i] = ch;
   }
+
+  //return decrypted value
   return decrypted;
 }
 
-//-------------------------------------------------------------
+//Main Function-------------------------------------------------------------
 
 int main(){
   char plaintext[100], encpt[100], decpt[100];
   int choose;
 
+  //Main Operation Menu
   do{
     printf("\n\nAtbash Cipher\n");
     printf("=================\n");
@@ -49,11 +59,14 @@ int main(){
     printf("[1] Encrypt\n");
     printf("[2] Decrypt\n");
     printf("=================\n");
+
+    //Initialize "choose" by selecting from Main Operation Menu
     printf("\nEnter Operation to Begin: ");
     scanf("%d", &choose);
 
-
+    //"Choose" switch case action
     switch(choose) {
+      //Case 1: Will send "plaintext" to "funcEncpt"
       case 1:
         fflush(stdin);
         printf("\nEnter Plaintext:\n");
@@ -61,11 +74,15 @@ int main(){
         strcpy(encpt, funcEncpt(plaintext));
         printf("Encrypted Value:\n%s", encpt);
         break;
+
+      //Case 2: Will send "encpt" to "funcDecpt"
       case 2:
         printf("\nEncrypted Value:\n%s\n", encpt);
         strcpy(decpt, funcEncpt(encpt));
         printf("Decrypted Value:\n%s", decpt);
         break;
+      
+      //Default: will terminate program if "choose" is not in Menu
       default:
         break;
     }
